70eb10df1a73470cbbca5dd088ddd9c0bc212b5f
Alex <t_tapak@yahoo.com>
2016-10-09 21:05:01 +0300

    actual data

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,33 +4,38 @@ import { HttpModule }    from "@angular/http"
 import { BrowserModule } from "@angular/platform-browser"
 
 import { MenuComponent }     from "./menu/menu.component"
+import { CardComponent }     from "./core/card/card.component"
 import { MenuService }       from "./menu/menu.service"
 import { routing }           from "./app-routing.module"
 import { AppComponent }      from "./app.component"
 import { PostsComponent }    from "./posts/posts.component"
-import { PostsService }      from "./posts/posts.service"
+import { PostService }       from "./posts/post.service"
 import { MenuItemComponent } from "./menu/menu-item/menu-item.component"
+import { PostEditComponent } from "./posts/post-edit/post-edit.component"
+import { CKEditorModule }    from "./core/ckeditor/ckeditor.module";
 
 @NgModule({
     imports     : [
         BrowserModule,
         FormsModule,
         HttpModule,
-        routing
+        routing,
+        CKEditorModule
     ],
     declarations: [
-        MenuComponent,
         AppComponent,
+        CardComponent,
+        MenuComponent,
+        MenuItemComponent,
         PostsComponent,
-        MenuItemComponent
+        PostEditComponent
     ],
     providers: [
         MenuService,
-        PostsService
+        PostService
     ],
     exports     : [],
     bootstrap   : [
-        MenuComponent,
         AppComponent
     ]
 })
