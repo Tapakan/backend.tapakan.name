d79177f2efaeebb1737fb6d5f73af05f66048c82
Alex <t_tapak@yahoo.com>
2016-10-09 21:04:40 +0300

    changed url template

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core"
 
 @Component({
     selector   : "eva-app",
-    templateUrl: "app/app.template.html",
+    templateUrl: "app/app.component.html",
     styleUrls  : [
         'app/app.component.css'
     ]
