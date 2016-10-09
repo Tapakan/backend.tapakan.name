513367919aebe32acaf66f211a6a75e03c76d13e
Alex <t_tapak@yahoo.com>
2016-10-09 21:02:44 +0300

    new route

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,17 +1,23 @@
-import { ModuleWithProviders }  from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { ModuleWithProviders }  from "@angular/core"
+import { Routes, RouterModule } from "@angular/router"
 
-import { PostsComponent } from "./posts/posts.component"
-import { AppComponent }   from "./app.component"
+import { PostsComponent }    from "./posts/posts.component"
+import { PostEditComponent } from "./posts/post-edit/post-edit.component"
 
 const AppRoutes: Routes = [
     {
-        path     : '',
-        component: AppComponent
+        path      : '',
+        redirectTo: 'posts',
+        pathMatch : 'full'
     },
     {
         path     : 'posts',
-        component: PostsComponent
+        component: PostsComponent,
+        pathMatch: 'full'
+    },
+    {
+        path     : 'posts/:id',
+        component: PostEditComponent
     }
 ];
 
